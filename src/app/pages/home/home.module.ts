b3a737ceec87acce491535600af0d60f70f8693d
Joseph Chingalo <profschingalo@gmail.com>
2021-04-06 11:54:11 +0300

    chore: initiate report view page

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -6,11 +6,11 @@ import { SharedModule } from 'src/app/shared/shared.module';
 import { pages } from './pages';
 import { OuSelectionComponent } from './components/ou-selection/ou-selection.component';
 import { PeSelectionComponent } from './components/pe-selection/pe-selection.component';
-import { DashboardChartComponent } from './components/dashboard-chart/dashboard-chart.component';
 import { LoaderComponent } from './components/loader/loader.component';
+import { ReportViewComponent } from './components/report-view/report-view.component';
 
 @NgModule({
-  declarations: [...pages, OuSelectionComponent, PeSelectionComponent, DashboardChartComponent, LoaderComponent],
+  declarations: [...pages, OuSelectionComponent, PeSelectionComponent, LoaderComponent, ReportViewComponent],
   imports: [CommonModule, SharedModule, HomeRoutingModule]
 })
 export class HomeModule {}
