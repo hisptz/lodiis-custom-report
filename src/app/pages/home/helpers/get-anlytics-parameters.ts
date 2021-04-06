42f8c49019f00148dde5f3379c9bf05d1d62434f
Joseph Chingalo <profschingalo@gmail.com>
2021-04-06 11:53:29 +0300

    chore: removed unsed items

--- src/app/pages/home/helpers/get-anlytics-parameters.ts
+++ src/app/pages/home/helpers/get-anlytics-parameters.ts
@@ -1,16 +1,16 @@
-import { DATA_CONFIG } from './get-chart-data-config';
+
 import * as _ from 'lodash';
 
 export function getAnlyticsParameters(
   selectedOrgUnitItems: any,
-  selectedPeriods: any
+  selectedPeriods: any,
 ) {
   const pe = _.uniq(
     _.flattenDeep(_.map(selectedPeriods, (period: any) => period.id || []))
   );
   const dx = _.uniq(
     _.flattenDeep(
-      _.map(DATA_CONFIG, (dataConfig: any) => {
+      _.map([], (dataConfig: any) => {
         return _.map(
           dataConfig.series || [],
           (seriesConfig: any) => seriesConfig.id || []
--- src/app/pages/home/helpers/get-dashboard-chart-selections.ts
+++ src/app/pages/home/helpers/get-dashboard-chart-selections.ts
@@ -5,14 +5,3 @@ export function getDefaultOrganisationUnitSelections(userOrgnisationUnits) {
     ? _.uniqBy(userOrgnisationUnits, 'id')
     : [];
 }
-
-export function getDefaultPeriodSelections(today?: Date) {
-  const date = today ? new Date(today) : new Date();
-  const monthIndex = date.getMonth();
-  const currentYear = date.getFullYear();
-  const id =
-    monthIndex >= 3 && monthIndex <= 8
-      ? `${currentYear - 1}AprilS2`
-      : `${currentYear}AprilS1`;
-  return [{ id, type: 'SixMonthlyApril' }];
-}
