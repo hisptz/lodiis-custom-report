5adae2000908024872b3918efa678bd39f55dac7
chingaloEric <echingalo@gmail.com>
2021-04-09 11:23:59 +0300

    refactor(loader): added indeterminate progress bar for when evaluating pagination

--- src/app/store/effects/report.effects.ts
+++ src/app/store/effects/report.effects.ts
@@ -16,7 +16,7 @@ import { State } from '../reducers';
 
 @Injectable()
 export class ReportDataEffects {
-  defaultAnalyticKeys  = ["eventdate","enrollmentdate","tei","ouname","ou"];
+  defaultAnalyticKeys = ['eventdate', 'enrollmentdate', 'tei', 'ouname', 'ou'];
 
   LoadReportData$ = createEffect(() =>
     this.actions$.pipe(
@@ -30,18 +30,19 @@ export class ReportDataEffects {
     )
   );
 
-  getReportAnalyticData(paremeters: any) {
-    const{ analyticParameters,reportConfig } = paremeters;
-    return  new Observable(observer=>{
-      this.getEventReportAnalyticData(analyticParameters,reportConfig ).then(data=>{
-        observer.next(data)
-        observer.complete()
-      }).catch(error=>observer.error(error))
-     
-    })
+  getReportAnalyticData(parameters: any) {
+    const { analyticParameters, reportConfig } = parameters;
+    return new Observable((observer) => {
+      this.getEventReportAnalyticData(analyticParameters, reportConfig)
+        .then((data) => {
+          observer.next(data);
+          observer.complete();
+        })
+        .catch((error) => observer.error(error));
+    });
   }
 
-  async getEventReportAnalyticData(analyticParameters :any,reportConfig:any ){
+  async getEventReportAnalyticData(analyticParameters: any, reportConfig: any) {
     const eventReportAnalyticData = [];
     const programId = reportConfig.program;
     const analyticData = [];
@@ -50,139 +51,227 @@ export class ReportDataEffects {
       let totalOverAllProcess = 0;
       let overAllProcessCount = 0;
       let bufferProcessCount = 0;
-      for(const analyticParameter of  analyticParameters){
-        const response :any = await this.getAnalyticParameterWithPaginationFilter(analyticParameter,programId);
+      for (const analyticParameter of analyticParameters) {
+        const response: any = await this.getAnalyticParameterWithPaginationFilter(
+          analyticParameter,
+          programId
+        );
         totalOverAllProcess += response.paginationFilters.length;
         analyticParametersWithPaginationFilters.push(response);
       }
-      for(const analyticParameter of  _.flattenDeep(analyticParametersWithPaginationFilters)){
-        const { url,stage, paginationFilters  } = analyticParameter;
+      for (const analyticParameter of _.flattenDeep(
+        analyticParametersWithPaginationFilters
+      )) {
+        const { url, stage, paginationFilters } = analyticParameter;
         bufferProcessCount += paginationFilters.length;
-        this.updateProgressStatus(bufferProcessCount,overAllProcessCount, totalOverAllProcess);
-        const response : any= await this.getSingleEventReportAnalyticData(url,stage,paginationFilters,bufferProcessCount,overAllProcessCount, totalOverAllProcess);
-        const {data:anlytics, stage :programStage, overAllProcessCount : currentOverAllProcessCount} = response;
+        this.updateProgressStatus(
+          bufferProcessCount,
+          overAllProcessCount,
+          totalOverAllProcess
+        );
+        const response: any = await this.getSingleEventReportAnalyticData(
+          url,
+          stage,
+          paginationFilters,
+          bufferProcessCount,
+          overAllProcessCount,
+          totalOverAllProcess
+        );
+        const {
+          data: anlytics,
+          stage: programStage,
+          overAllProcessCount: currentOverAllProcessCount,
+        } = response;
         overAllProcessCount = currentOverAllProcessCount;
-        const sanitizedResponse = this.getSanitizedAnalyticData(anlytics,programStage);
+        const sanitizedResponse = this.getSanitizedAnalyticData(
+          anlytics,
+          programStage
+        );
         analyticData.push(sanitizedResponse);
       }
-      const formattedEventReportData = this.getFormattedEventAnalyticDataForReport(_.flattenDeep(analyticData), reportConfig);
+      const formattedEventReportData = this.getFormattedEventAnalyticDataForReport(
+        _.flattenDeep(analyticData),
+        reportConfig
+      );
       eventReportAnalyticData.push(formattedEventReportData);
     } catch (error) {
-      console.log({error});
+      console.log({ error });
     }
     return _.flattenDeep(eventReportAnalyticData);
   }
 
-
-  async getSingleEventReportAnalyticData(url : string, stage:string, paginationFilters:any, bufferProcessCount : number,overAllProcessCount: number,  totalOverAllProcess: number){
+  async getSingleEventReportAnalyticData(
+    url: string,
+    stage: string,
+    paginationFilters: any,
+    bufferProcessCount: number,
+    overAllProcessCount: number,
+    totalOverAllProcess: number
+  ) {
     let analyticData = {};
     try {
-      for(const paginationFilter of paginationFilters){
-        const data:any = await this.getAnalyticResult(url,paginationFilter);
-        overAllProcessCount ++;
-        this.updateProgressStatus(bufferProcessCount,overAllProcessCount, totalOverAllProcess);
-        if(_.keys(analyticData).length === 0){
-          analyticData = {...analyticData, ...data};
-        }else{
-          const rows = _.concat(analyticData["rows"] || [], data["rows"]||[]);
-          analyticData = {...analyticData,rows}
+      for (const paginationFilter of paginationFilters) {
+        const data: any = await this.getAnalyticResult(url, paginationFilter);
+        overAllProcessCount++;
+        this.updateProgressStatus(
+          bufferProcessCount,
+          overAllProcessCount,
+          totalOverAllProcess
+        );
+        if (_.keys(analyticData).length === 0) {
+          analyticData = { ...analyticData, ...data };
+        } else {
+          const rows = _.concat(analyticData['rows'] || [], data['rows'] || []);
+          analyticData = { ...analyticData, rows };
         }
       }
     } catch (error) {
-      console.log({error});
+      console.log({ error });
     }
-    return {data  : analyticData, stage, overAllProcessCount};
+    return { data: analyticData, stage, overAllProcessCount };
   }
 
-  async getAnalyticParameterWithPaginationFilter(analyticParameter : any, programId:string){
+  async getAnalyticParameterWithPaginationFilter(
+    analyticParameter: any,
+    programId: string
+  ) {
     const paginationFilters = [];
     const pe = _.join(analyticParameter.pe || [], ';');
     const ou = _.join(analyticParameter.ou || [], ';');
-    const stage = _.map(analyticParameter.dx || [], (dx:string)=> dx.split('.')[0])[0];
-    const dataDimension = _.join(_.map(analyticParameter.dx || [], (dx:string)=>`dimension=${dx}` ), '&');
+    const stage = _.map(
+      analyticParameter.dx || [],
+      (dx: string) => dx.split('.')[0]
+    )[0];
+    const dataDimension = _.join(
+      _.map(analyticParameter.dx || [], (dx: string) => `dimension=${dx}`),
+      '&'
+    );
     const pageSize = 500;
     const url = `analytics/events/query/${programId}.json?dimension=pe:${pe}&dimension=ou:${ou}&${dataDimension}&stage=${stage}&displayProperty=NAME&outputType=EVENT&desc=eventdate`;
     try {
-      const response :any = await this.getPaginatinationFilters(url,pageSize);
-      paginationFilters.push(response)
+      const response: any = await this.getPaginatinationFilters(url, pageSize);
+      paginationFilters.push(response);
     } catch (error) {
-      console.log({error});
+      console.log({ error });
     }
-    return {...analyticParameter, url,stage, paginationFilters : _.flattenDeep(paginationFilters)}
+    return {
+      ...analyticParameter,
+      url,
+      stage,
+      paginationFilters: _.flattenDeep(paginationFilters),
+    };
   }
 
-  getAnalyticResult(url:string , paginationFilter:string ){
-    return new Promise((resolve, reject)=>{
-        this.httpClient.get(`${url}&${paginationFilter}`).subscribe(data=> resolve(data), error=> reject(error));
-      });
+  getAnalyticResult(url: string, paginationFilter: string) {
+    return new Promise((resolve, reject) => {
+      this.httpClient.get(`${url}&${paginationFilter}`).subscribe(
+        (data) => resolve(data),
+        (error) => reject(error)
+      );
+    });
   }
 
-  getPaginatinationFilters(url:string, pageSize :number){
+  getPaginatinationFilters(url: string, pageSize: number) {
     const paginationFilters = [];
-    return new Promise((resolve, reject)=>{
-      this.httpClient.get(`${url}&pageSize=1&page=1`).subscribe(anlytics=> {
-        const {metaData} = anlytics;
-        const pager = metaData && metaData.pager ?metaData.pager : {};
-        const total = pager.total || pageSize;
-        for (let page = 1; page <= Math.ceil(total / pageSize); page++) {
-          paginationFilters.push(`pageSize=${pageSize}&page=${page}`);
-        }
-        resolve(paginationFilters);
-      }, error=> reject(error));
-    })
+    return new Promise((resolve, reject) => {
+      this.httpClient.get(`${url}&pageSize=1&page=1`).subscribe(
+        (anlytics) => {
+          const { metaData } = anlytics;
+          const pager = metaData && metaData.pager ? metaData.pager : {};
+          const total = pager.total || pageSize;
+          for (let page = 1; page <= Math.ceil(total / pageSize); page++) {
+            paginationFilters.push(`pageSize=${pageSize}&page=${page}`);
+          }
+          resolve(paginationFilters);
+        },
+        (error) => reject(error)
+      );
+    });
   }
 
-  getFormattedEventAnalyticDataForReport(analyticData :Array<any>,reportConfig:any ){
-    const groupedAnalyticDataByBeneficiary = _.groupBy(analyticData, "tei");
-    return _.flattenDeep(_.map(_.keys(groupedAnalyticDataByBeneficiary), (tei:string)=>{
-      const analyticDataByBeneficiary = groupedAnalyticDataByBeneficiary[tei];
-      const beneficiaryData = {};
-      for(const dxConfig of reportConfig.dxConfig || []){
-        const{ id,name,programStage,isBoolean,code,isDate} = dxConfig;
-        const eventReportData = id !=="" && programStage === "" ? 
-        _.find(analyticDataByBeneficiary, (data:any)=> {
-          return _.keys(data).includes(id);
-        }) : _.find(analyticDataByBeneficiary, (data:any)=> {
-          return _.keys(data).includes(id) && data["programStage"] && data["programStage"] === programStage;
-        });
-        let value = eventReportData ? eventReportData[id] : "";
-        if(_.keys(beneficiaryData).includes(name) && beneficiaryData[name] !== ""){
-          value = beneficiaryData[name];
+  getFormattedEventAnalyticDataForReport(
+    analyticData: Array<any>,
+    reportConfig: any
+  ) {
+    const groupedAnalyticDataByBeneficiary = _.groupBy(analyticData, 'tei');
+    return _.flattenDeep(
+      _.map(_.keys(groupedAnalyticDataByBeneficiary), (tei: string) => {
+        const analyticDataByBeneficiary = groupedAnalyticDataByBeneficiary[tei];
+        const beneficiaryData = {};
+        for (const dxConfig of reportConfig.dxConfig || []) {
+          const { id, name, programStage, isBoolean, code, isDate } = dxConfig;
+          const eventReportData =
+            id !== '' && programStage === ''
+              ? _.find(analyticDataByBeneficiary, (data: any) => {
+                  return _.keys(data).includes(id);
+                })
+              : _.find(analyticDataByBeneficiary, (data: any) => {
+                  return (
+                    _.keys(data).includes(id) &&
+                    data['programStage'] &&
+                    data['programStage'] === programStage
+                  );
+                });
+          let value = eventReportData ? eventReportData[id] : '';
+          if (
+            _.keys(beneficiaryData).includes(name) &&
+            beneficiaryData[name] !== ''
+          ) {
+            value = beneficiaryData[name];
+          }
+          beneficiaryData[name] =
+            value !== ''
+              ? this.getSanitizesValue(value, code, isBoolean, isDate)
+              : value;
         }
-        beneficiaryData[name] = value !== ""?  this.getSanitizesValue(value,code, isBoolean,isDate): value;
-      }
-      return beneficiaryData;
-    }));
+        return beneficiaryData;
+      })
+    );
   }
 
-  getSanitizesValue(value :any, code:Array<string>, isBoolean :boolean, isDate:boolean){
-    let sanitizedValue = "";
-    if(code && code.length > 0){
-      sanitizedValue = code.includes(value) ? "Yes" : sanitizedValue;
-    }else if(isBoolean){
-      sanitizedValue = `${value}` === "1" ? "Yes" : sanitizedValue;
-    }else if(isDate){
+  getSanitizesValue(
+    value: any,
+    code: Array<string>,
+    isBoolean: boolean,
+    isDate: boolean
+  ) {
+    let sanitizedValue = '';
+    if (code && code.length > 0) {
+      sanitizedValue = code.includes(value) ? 'Yes' : sanitizedValue;
+    } else if (isBoolean) {
+      sanitizedValue = `${value}` === '1' ? 'Yes' : sanitizedValue;
+    } else if (isDate) {
       sanitizedValue = this.getFormattedDate(value);
-    }
-    else{
+    } else {
       sanitizedValue = value;
     }
     return sanitizedValue;
   }
 
-  updateProgressStatus(bufferProcessCount : number,overAllProcessCount: number, totalOverAllProcess: number){
-    const bufferProgress = this.getProgressPercentage(bufferProcessCount, totalOverAllProcess);
-    const overAllProgress = this.getProgressPercentage(overAllProcessCount, totalOverAllProcess);
-    this.store.dispatch(UpdateReportProgress({overAllProgress,bufferProgress}));
+  updateProgressStatus(
+    bufferProcessCount: number,
+    overAllProcessCount: number,
+    totalOverAllProcess: number
+  ) {
+    const bufferProgress = this.getProgressPercentage(
+      bufferProcessCount,
+      totalOverAllProcess
+    );
+    const overAllProgress = this.getProgressPercentage(
+      overAllProcessCount,
+      totalOverAllProcess
+    );
+    this.store.dispatch(
+      UpdateReportProgress({ overAllProgress, bufferProgress })
+    );
   }
 
-  getProgressPercentage(numerator:number, denominator:number){
-    const percentageValue = ((numerator/denominator) *100).toFixed(0);
-    return parseInt(percentageValue,10);
+  getProgressPercentage(numerator: number, denominator: number) {
+    const percentageValue = ((numerator / denominator) * 100).toFixed(0);
+    return parseInt(percentageValue, 10);
   }
 
-
-  getFormattedDate(date :any) {
+  getFormattedDate(date: any) {
     let dateObject = new Date(date);
     if (isNaN(dateObject.getDate())) {
       dateObject = new Date();
@@ -195,28 +284,36 @@ export class ReportDataEffects {
       (month > 9 ? `-${month}` : `-0${month}`) +
       (day > 9 ? `-${day}` : `-0${day}`)
     );
-}
+  }
 
-  getSanitizedAnalyticData(anlytics:any,programStage:string){
-    const {headers, rows,metaData} = anlytics;
-    const dimensions = metaData && metaData.dimensions ?metaData.dimensions : {};
-    const defaultKeys = _.flattenDeep(_.concat(this.defaultAnalyticKeys, _.keys(_.omit(dimensions,_.concat(["ou",'pe',], dimensions.ou || [])))))
-    return _.flattenDeep(_.map(rows, (rowData:any)=>{
-      const dataObject = {"programStage" : programStage};
-      for(const key of defaultKeys){
-        const keyIndex = _.findIndex(
-          headers || [],
-          (header :any) => header && header.name === key
-        );
-        dataObject[key] = rowData[keyIndex] || "";
-      }
-      return dataObject;
-    }));
+  getSanitizedAnalyticData(anlytics: any, programStage: string) {
+    const { headers, rows, metaData } = anlytics;
+    const dimensions =
+      metaData && metaData.dimensions ? metaData.dimensions : {};
+    const defaultKeys = _.flattenDeep(
+      _.concat(
+        this.defaultAnalyticKeys,
+        _.keys(_.omit(dimensions, _.concat(['ou', 'pe'], dimensions.ou || [])))
+      )
+    );
+    return _.flattenDeep(
+      _.map(rows, (rowData: any) => {
+        const dataObject = { programStage: programStage };
+        for (const key of defaultKeys) {
+          const keyIndex = _.findIndex(
+            headers || [],
+            (header: any) => header && header.name === key
+          );
+          dataObject[key] = rowData[keyIndex] || '';
+        }
+        return dataObject;
+      })
+    );
   }
 
   constructor(
     private actions$: Actions,
     private httpClient: NgxDhis2HttpClientService,
-    private store: Store<State>,
+    private store: Store<State>
   ) {}
 }
