9fa2a8d626bb4b580108c3be539b8f93b901494f
chingaloEric <echingalo@gmail.com>
2021-05-01 17:56:22 +0300

    refactor(report-name): refactored the report name for the generated reports

--- src/app/pages/home/pipes/report-name.pipe.ts
+++ src/app/pages/home/pipes/report-name.pipe.ts
@@ -6,6 +6,9 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class ReportNamePipe implements PipeTransform {
   transform(reportName: string, date = ''): string {
     const sanitizedDate = date.split('T')[0];
-    return `${reportName}_${sanitizedDate}`;
+    const reportNamesSections = reportName.split('.');
+    return `${reportNamesSections[0] ?? ''}_${sanitizedDate ?? ''}.${
+      reportNamesSections[1] ?? ''
+    }`;
   }
 }
