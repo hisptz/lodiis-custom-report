62a1ea771c437d36eaec229868f86b69e8a91ce5
Joseph Chingalo <profschingalo@gmail.com>
2021-10-05 17:04:15 +0300

    feat: support for age, age range and age ranges fields

--- src/app/shared/helpers/get-formatted-analytica-data-for-report.ts
+++ src/app/shared/helpers/get-formatted-analytica-data-for-report.ts
@@ -1,5 +1,4 @@
 import * as _ from 'lodash';
-import { types } from 'util';
 import {
   getSanitizesReportValue,
   getSanitizedDisplayValue,
@@ -10,11 +9,137 @@ const commmunityCouncilLevel = 3;
 const facilityLevel = 4;
 
 const noneAgywParticipationProgramStages = ['uctHRP6BBXP'];
+const beneficiaryDateOfBirthReference = 'qZP982qpSPS';
+
+function getLastServiceFromAnalyticData(
+  analyticDataByBeneficiary: Array<any>,
+  programStage: string
+) {
+  let lastService = {};
+  const sortedServices = _.reverse(
+    _.sortBy(
+      _.filter(
+        programStage && programStage !== ''
+          ? _.filter(
+              analyticDataByBeneficiary,
+              (data: any) =>
+                data.programStage && data.programStage === programStage
+            )
+          : analyticDataByBeneficiary,
+        (data: any) => data && data.hasOwnProperty('eventdate')
+      ),
+      ['eventdate']
+    )
+  );
+  if (sortedServices.length > 0) {
+    lastService = { ...lastService, ...sortedServices[0] };
+  }
+  return lastService;
+}
+
+function getLocationNameByLevel(
+  analyticDataByBeneficiary: Array<any>,
+  locations: Array<any>,
+  level: any
+) {
+  const ouIds = _.uniq(
+    _.flattenDeep(_.map(analyticDataByBeneficiary, (data) => data.ou || []))
+  );
+  const locationId = ouIds.length > 0 ? ouIds[0] : '';
+  return getLocationNameById(locations, level, locationId);
+}
+
+function getLocationNameById(
+  locations: Array<any>,
+  level: number,
+  locationId: string
+) {
+  let locationName = '';
+  const locationObj = _.find(
+    locations,
+    (data: any) => data && data.id && data.id === locationId
+  );
+  if (locationObj && locationObj.ancestors) {
+    const location = _.find(
+      locationObj.ancestors || [],
+      (data: any) => data && data.level === level
+    );
+    locationName = location ? location.name || locationName : locationName;
+  }
+  return locationName;
+}
+
+function getBeneficiaryAge(dob: string) {
+  var ageDifMs = Date.now() - new Date(dob).getTime();
+  var ageDate = new Date(ageDifMs);
+  return Math.abs(ageDate.getUTCFullYear() - 1970);
+}
+
+function getValueFromAnalyticalData(
+  analyticData: Array<any>,
+  id: string,
+  programStage: string
+) {
+  let value = '';
+  for (const data of _.filter(
+    analyticData || [],
+    (dataObjet: any) =>
+      dataObjet.programStage &&
+      (dataObjet.programStage === programStage || programStage === '')
+  )) {
+    value = data.hasOwnProperty(id) && `${data[id]}` !== '' ? data[id] : value;
+  }
+  return value;
+}
+
+function getBeneficiaryAgeRanges(age: number) {
+  let value =
+    age < 1
+      ? ''
+      : age >= 1 && age < 5
+      ? '1-4'
+      : age < 10
+      ? '5-9'
+      : age < 15
+      ? '10-14'
+      : age < 18
+      ? '15-17'
+      : age < 21
+      ? '18-20'
+      : '20+';
+  return value;
+}
+
+function getBeneficiaryAgeRange(age: number): string {
+  return age < 18 ? '0-17' : '18+';
+}
+
+function getBeneficiaryHivRiskAssessmentResult(
+  ids: any,
+  analyticDataByBeneficiary: any
+) {
+  let riskValue = 'Low risk';
+  for (const referenceId of ids || []) {
+    const referenceValue = getValueFromAnalyticalData(
+      analyticDataByBeneficiary,
+      beneficiaryDateOfBirthReference,
+      referenceId
+    );
+    if (
+      `${referenceValue}`.toLocaleLowerCase() === 'yes' ||
+      `${referenceValue}`.toLocaleLowerCase() === 'true'
+    ) {
+      riskValue = 'High risk';
+    }
+  }
+  return riskValue;
+}
 
 export function getFormattedEventAnalyticDataForReport(
   analyticData: Array<any>,
   reportConfig: any,
-  locations: any
+  locations: any,
+  programToProgramStageObject: any
 ) {
   const groupedAnalyticDataByBeneficiary = _.groupBy(analyticData, 'tei');
   return _.flattenDeep(
@@ -36,6 +161,7 @@ export function getFormattedEventAnalyticDataForReport(
       for (const dxConfigs of reportConfig.dxConfigs || []) {
         const {
           id,
+          ids,
           name,
           programStage,
           isBoolean,
@@ -45,16 +171,78 @@ export function getFormattedEventAnalyticDataForReport(
           displayValues,
         } = dxConfigs;
         let value = '';
-        //@TODO manipulate dob attributes to qZP982qpSPS
-        // beneficiary_age
-        //beneficiary_age_range
-        //beneficiary_age_ranges
-        //@TODO using programs to deduce types => ["em38qztTI8s", "BNsDaCclOiu"]
-        //beneficiary_type
+        if (isOrganisationUnit) {
+          // console.log({
+          //   id,
+          //   analyticDataByBeneficiary,
+          //   lo: 'wv3YAGLZlev',
+          //   locations,
+          // });
+        } else if (id === 'hiv_risk_assessment_result') {
+          value = getBeneficiaryHivRiskAssessmentResult(
+            ids,
+            analyticDataByBeneficiary
+          );
+        } else if (id === 'beneficiary_age') {
+          const dob = getValueFromAnalyticalData(
+            analyticDataByBeneficiary,
+            beneficiaryDateOfBirthReference,
+            programStage
+          );
+          if (dob !== '') {
+            const age = getBeneficiaryAge(dob);
+            value = `${age}`;
+          }
+        } else if (id === 'beneficiary_age_range') {
+          const dob = getValueFromAnalyticalData(
+            analyticDataByBeneficiary,
+            beneficiaryDateOfBirthReference,
+            programStage
+          );
+          if (dob !== '') {
+            const age = getBeneficiaryAge(dob);
+            value = getBeneficiaryAgeRange(age);
+          }
+        } else if (id === 'beneficiary_age_ranges') {
+          const dob = getValueFromAnalyticalData(
+            analyticDataByBeneficiary,
+            beneficiaryDateOfBirthReference,
+            programStage
+          );
+          if (dob !== '') {
+            const age = getBeneficiaryAge(dob);
+            value = getBeneficiaryAgeRanges(age);
+          }
+        } else if (id === 'beneficiary_type') {
+          const eventProgramStages = _.uniq(
+            _.flattenDeep(
+              _.map(analyticDataByBeneficiary || [], (data) =>
+                data && data.hasOwnProperty('programStage')
+                  ? data.programStage
+                  : []
+              )
+            )
+          );
+          let beneficiaryProgrmId = '';
+          if (eventProgramStages.length > 0) {
+            const stageId = eventProgramStages[0];
+            for (const programId in _.keys(eventProgramStages)) {
+              if (eventProgramStages[programId].includes(stageId)) {
+                beneficiaryProgrmId = programId;
+              }
+            }
+          }
 
-        //checking for organisation units fielfs
-        //wv3YAGLZlev
-        if (id === 'is_service_provided') {
+          // em38qztTI8s == OVC
+          // BNsDaCclOiu == caregiver
+          // hOEIHJDrrvz = AGYW/Dreams
+          //console.log({ id, analyticDataByBeneficiary });
+          console.log({
+            programToProgramStageObject,
+            eventProgramStages,
+            beneficiaryProgrmId,
+          });
+        } else if (id === 'is_service_provided') {
           const lastService = getLastServiceFromAnalyticData(
             analyticDataByBeneficiary,
             programStage
@@ -143,62 +331,4 @@ export function getFormattedEventAnalyticDataForReport(
       return beneficiaryData;
     })
   );
-}
-
-function getLastServiceFromAnalyticData(
-  analyticDataByBeneficiary: Array<any>,
-  programStage: string
-) {
-  let lastService = {};
-  const sortedServices = _.reverse(
-    _.sortBy(
-      _.filter(
-        programStage && programStage !== ''
-          ? _.filter(
-              analyticDataByBeneficiary,
-              (data: any) =>
-                data.programStage && data.programStage === programStage
-            )
-          : analyticDataByBeneficiary,
-        (data: any) => data && data.hasOwnProperty('eventdate')
-      ),
-      ['eventdate']
-    )
-  );
-  if (sortedServices.length > 0) {
-    lastService = { ...lastService, ...sortedServices[0] };
-  }
-  return lastService;
-}
-
-function getLocationNameByLevel(
-  analyticDataByBeneficiary: Array<any>,
-  locations: Array<any>,
-  level: any
-) {
-  const ouIds = _.uniq(
-    _.flattenDeep(_.map(analyticDataByBeneficiary, (data) => data.ou || []))
-  );
-  const locationId = ouIds.length > 0 ? ouIds[0] : '';
-  return getLocationNameById(locations, level, locationId);
-}
-
-function getLocationNameById(
-  locations: Array<any>,
-  level: number,
-  locationId: string
-) {
-  let locationName = '';
-  const locationObj = _.find(
-    locations,
-    (data: any) => data && data.id && data.id === locationId
-  );
-  if (locationObj && locationObj.ancestors) {
-    const location = _.find(
-      locationObj.ancestors || [],
-      (data: any) => data && data.level === level
-    );
-    locationName = location ? location.name || locationName : locationName;
-  }
-  return locationName;
 }
