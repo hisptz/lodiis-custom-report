1ecade1bfa6239d32830b9f471e75c4715cc011a
Danford Kija <kijadanford@gmail.com>
2022-08-19 10:30:49 +0300

    remove unintended fields into report column

--- src/app/core/constant/index.ts
+++ src/app/core/constant/index.ts
@@ -1,7 +1,12 @@
 export const MANDATORY_COLUMNS: string[] = [
-  // 'First Name',
-  // 'Surname',
-  // 'Date of Birth',
-  // 'Sex',
-  // 'Age',
+  'First Name',
+  'Surname',
+  'Date of Birth',
+  'Sex',
+  'Age',
+];
+
+export const OPTIONAL_COLUMNS: string[] = [
+  'Type of intervention',
+  'Number of session',
 ];
