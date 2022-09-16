0b5e6e58faad84b75d4a9aaf26ba4280bfe3ed1c
Joseph Chingalo <profschingalo@gmail.com>
2022-09-16 11:30:26 +0300

    feat: 🎸 link support for evaluate sppc and ppc on dreams/agyws

--- src/app/pages/home/helpers/secondary-primary-package-completion-helper.ts
+++ src/app/pages/home/helpers/secondary-primary-package-completion-helper.ts
@@ -1,9 +1,16 @@
 import { getDreamServiceLayeringAgeBand } from './beneficiary-helper';
 
+//hiv test or hts => stage : vAMc8n0YB6m =>  zbHpXUjGv5H
+
 export function evaluationOfSecondaryPrimaryPackageCompletion(
-  analyticDataByBeneficiary: any[]
+  analyticDataByBeneficiary: any[],
+  progranStages: Array<{
+    id: string;
+    dataElements: string[];
+  }>
 ): string {
   let completed = 'No';
+  console.log({ progranStages, type: 'sppc' });
   const ageBand = getDreamServiceLayeringAgeBand(analyticDataByBeneficiary);
   switch (ageBand) {
     case '10-14': {
