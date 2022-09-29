aa660b36893ef36187ed60f2ef0298b667b72c78
Joseph Chingalo <profschingalo@gmail.com>
2022-09-29 10:48:59 +0300

    feat: 🎸 support get evaluated criteria for SPC

--- src/app/pages/home/helpers/secondary-primary-package-completion-helper.ts
+++ src/app/pages/home/helpers/secondary-primary-package-completion-helper.ts
@@ -8,43 +8,45 @@ import {
 const htsProgramStage = 'vAMc8n0YB6m';
 const serviceFormProgramStage = 'bDJq2JWVTbC';
 const condomEducationProgramStage = 'NXsIkG9Q1BA';
+const referralProgramStage = 'MkyTrLeBG8I';
+const parentingProgramStage = 'N5SlNqQuMyC';
+const familyPlanningProgramStage = 'A7Tl3vML6as';
+const postGbvClinicalProgramStage = 'yK0ENCuwPqh';
+const postGbvLegalProgramStage = 'VLW93YjZOyf';
+const prepShortFormProgramStage = 'Qw8c20q5V0w';
+const prepLongFormProgramStage = 'mMjGlK1W0Xo';
 
 const interventionReference = 'Eug4BXDFLym';
 const condomEducationReference = 'd4AJf9yiKpL';
 const hivTestReference = 'zbHpXUjGv5H';
-
-//Prep long forms :: mMjGlK1W0Xo
-// de : hiv neg => veoA322323t, < 60 => bH9DpJOIutM, weight => h0P6UfkUvLP
-//Prep short forms :: Qw8c20q5V0w => VtmkYCQkBQw
-
-// post gbv clinical :: yK0ENCuwPqh => mnYT2rZyGgJ
-// post gbv legal :: VLW93YjZOyf => qML4gVZ2UFc
-
-// parenting :: N5SlNqQuMyC => NhUVtfObJFw
-
-// family planning/srh :: A7Tl3vML6as => uciT2F6ByYO
-
-//Referrals :: MkyTrLeBG8I
-// at facility => OrC9Bh2bcFz
-// at community => rsh5Kvx6qAU
-// service provided => hXyqgOWZ17b
+const parentingReference = 'NhUVtfObJFw';
+const familyPlanningReference = 'uciT2F6ByYO';
+const postGbvClinicalReference = 'mnYT2rZyGgJ';
+const postGbvLegalReference = 'qML4gVZ2UFc';
+const referralServiceProvidedReference = 'hXyqgOWZ17b';
+const referralAtFacility = 'OrC9Bh2bcFz';
+const referralAtCommunity = 'rsh5Kvx6qAU';
+const prepShortFormReference = 'VtmkYCQkBQw';
+const prepHivNegativeReference = 'veoA322323t';
+const prepCreatinineClearanceReference = 'bH9DpJOIutM';
+const prepWeightReference = 'h0P6UfkUvLP';
 
 export function evaluationOfSecondaryPrimaryPackageCompletion(
   analyticDataByBeneficiary: any[],
-  progranStages: Array<{
+  programStages: Array<{
     id: string;
     dataElements: string[];
   }>
 ): string {
   let completed = 'No';
-  const progranStageIds = _.flattenDeep(
-    _.map(progranStages, (progranStage: any) => progranStage.id || [])
+  const programStageIds = _.flattenDeep(
+    _.map(programStages, (programStage: any) => programStage.id || [])
   );
   const beneficiaryServiceData = _.filter(
     analyticDataByBeneficiary,
     (beneficiaryData: any) => {
       const programStageId = beneficiaryData['programStage'] || '';
-      return progranStageIds.includes(programStageId);
+      return programStageIds.includes(programStageId);
     }
   );
   if (beneficiaryServiceData.length > 0) {
@@ -61,6 +63,74 @@ export function evaluationOfSecondaryPrimaryPackageCompletion(
       condomEducationReference,
       `1`
     );
+    const hasPostGbvClinicalProvided = getStatusFromBeneficiarySericeData(
+      beneficiaryServiceData,
+      postGbvClinicalProgramStage,
+      postGbvClinicalReference,
+      `1`
+    );
+    const hasPostGbvlegalProvided = getStatusFromBeneficiarySericeData(
+      beneficiaryServiceData,
+      postGbvLegalProgramStage,
+      postGbvLegalReference,
+      `1`
+    );
+    const hasParentingProvided = getStatusFromBeneficiarySericeData(
+      beneficiaryServiceData,
+      parentingProgramStage,
+      parentingReference,
+      `1`
+    );
+    const hasFamilyPlanningProvided = getStatusFromBeneficiarySericeData(
+      beneficiaryServiceData,
+      familyPlanningProgramStage,
+      familyPlanningReference,
+      `1`
+    );
+    const hasPrepShortFormProvided = getStatusFromBeneficiarySericeData(
+      beneficiaryServiceData,
+      prepShortFormProgramStage,
+      prepShortFormReference,
+      `1`
+    );
+    const hasPrepHivNegative = getStatusFromBeneficiarySericeData(
+      beneficiaryServiceData,
+      prepLongFormProgramStage,
+      prepHivNegativeReference,
+      `1`
+    );
+    const hasPrepCreatinineClearance = getStatusFromBeneficiarySericeData(
+      beneficiaryServiceData,
+      prepLongFormProgramStage,
+      prepCreatinineClearanceReference,
+      `1`
+    );
+    const hasPrepWeightBelow = getStatusFromBeneficiarySericeData(
+      beneficiaryServiceData,
+      prepLongFormProgramStage,
+      prepWeightReference,
+      `1`
+    );
+    const hasReferralServiceProvided = getStatusFromBeneficiarySericeData(
+      beneficiaryServiceData,
+      referralProgramStage,
+      referralServiceProvidedReference,
+      `1`
+    );
+    const educationSupportAtFacilityReferralCount =
+      getSessionCountOnDreamsService(
+        beneficiaryServiceData,
+        referralProgramStage,
+        'EducationSubsidiesSupport',
+        referralAtFacility
+      );
+    const educationSupportAtCommunityReferralCount =
+      getSessionCountOnDreamsService(
+        beneficiaryServiceData,
+        referralProgramStage,
+        'EducationSubsidiesSupport',
+        referralAtCommunity
+      );
     const condomEducationSessions = getSessionCountOnDreamsService(
       beneficiaryServiceData,
       serviceFormProgramStage,
